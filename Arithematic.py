93d98a9b57cb86ce35677fc3697da9d826ca8bb8
Alunkar <57435267+Alunkar@users.noreply.github.com>
2019-11-06 16:59:07 +0530

    Arithmetic Operations

---
+++ Arithematic.py
@@ -0,0 +1,19 @@
+num1=int(input("enter number"));
+num2=int(input("enter number"));
+
+def Add():
+	Ans=int(num1)+int(num2);
+	print("Addition is: ", Ans);
+
+def Subs():
+	Ans=int(num1)-int(num2);
+	print("Substraction is: ", Ans);
+	
+def Multi():
+	Ans=int(num1)*int(num2);
+	print("Product is: ", Ans);
+
+def Divide():
+	Ans=int(num1)/int(num2);
+	print("Division is: ", Ans);
+
---
+++ Main Program.py
@@ -0,0 +1,13 @@
+import Arithematic
+def fun():
+	Arithematic.Add();
+	Arithematic.Subs();
+	Arithematic.Multi();
+	Arithematic.Divide();
+
+
+def main():
+	fun();
+
+if __name__ == "__main__":
+	main();
