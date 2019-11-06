7b662159d17f8756d1947ae418ec5955de72ac1d
Alunkar <57435267+Alunkar@users.noreply.github.com>
2019-11-06 17:08:42 +0530

    Numbered Pattern

---
+++ Pattern Numbered.py
@@ -0,0 +1,19 @@
+def NumPat():
+	x=int(input("Enter number: "))
+	for j in range(x):
+		for i in range(x):
+			print((i+1),end=' ');
+		print();
+
+
+
+
+
+
+
+
+def main():
+	NumPat();
+
+if __name__ == "__main__":
+	main();
