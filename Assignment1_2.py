ac2d386557c6b0f66142a3e874b97e89438f8210
Alunkar <57435267+Alunkar@users.noreply.github.com>
2019-11-06 16:46:02 +0530

    Even Odd

---
+++ Assignment1_2.py
@@ -0,0 +1,15 @@
+def ChkNum():
+	num= int(input("Enter Number"));
+	if num%2 == 0:
+			print("Number is Even");
+	else:
+			print("Number is odd");
+
+
+
+def main():
+	ChkNum();
+
+
+if __name__ == "__main__":
+	main();
