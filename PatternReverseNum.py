5b6fff3290bc38d2ca73d1c6f3b6e943a6a2b3be
Alunkar <57435267+Alunkar@users.noreply.github.com>
2019-11-06 17:12:30 +0530

    Number pattern in reverse pyramid

---
+++ PatternReverseNum.py
@@ -0,0 +1,19 @@
+def NumPat():
+	x=int(input("Enter number: "))
+	for j in range(x):
+		for i in range(j+1):
+			print((i+1),end=' ');
+		print();
+
+
+
+
+
+
+
+
+def main():
+	NumPat();
+
+if __name__ == "__main__":
+	main();
